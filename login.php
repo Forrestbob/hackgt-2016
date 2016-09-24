9e15fd5e04e441e113bd36f7fcf272d685c38ea7
Forrestbob <forrestbob2000@yahoo.com>
2016-09-24 13:09:35 -0400

    update

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="login_style.css">
+</head>
+
+
+<body>
+	<img id = "logo" src="images/delta.png">
+</body>
+
+
+</html>
