cec6ddb7dee215d1a060d18d8b97f7c6395954f3
Forrestbob <forrestbob2000@yahoo.com>
2016-09-24 15:01:52 -0400

    login form

--- login.php
+++ login.php
@@ -2,11 +2,25 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="login_style.css">
+	<title>Login | Delta</title>
 </head>
 
 
 <body>
 	<img id = "logo" src="images/delta.png">
+	
+	<div id = "login_form">
+		<form action="login.php"  method = "GET">
+			Username:<br>
+			<input type = "text" name = "username"><br>
+			
+			Password:<br>
+			<input type = "password" name = "password">
+			<br><br>
+			<input type = "submit" name = "submit">
+		</form>
+	</div>
+	
 </body>
 
 
