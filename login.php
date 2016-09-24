8aff06c37b7bc5a2a87595a4581715b28ed8a073
Lawrence Chen <the_Crimson_chen@yahoo.com>
2016-09-24 17:55:46 -0400

    basically finished login page

--- login.php
+++ login.php
@@ -11,13 +11,19 @@
 	
 	<div id = "login_form">
 		<form action="login.php"  method = "GET">
-			Username:<br>
-			<input type = "text" name = "username"><br>
+
+			<h2 align="center"><span id = "login_text">Login to My Delta</span></h2>
+
+			<span id= "all_fields">All fields are required. </span><br><br>
+
+			<span id = "custom_word">Username:</span><br>
+			<input type = "text" name = "username" placeholder="Enter SkyMiles number or username"><br>
 			
-			Password:<br>
-			<input type = "password" name = "password">
+			<span id = "custom_word">Password:</span><br>
+			<input type = "password" name = "password" placeholder="Enter password">
 			<br><br>
-			<input type = "submit" name = "submit">
+			
+			<input type = "submit" name = "submit" value = "Log in">
 		</form>
 	</div>
 	
