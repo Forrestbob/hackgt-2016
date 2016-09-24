bbb7d0b4474d0e41657bffce62a286f35dbd5c1c
Lawrence Chen <the_Crimson_chen@yahoo.com>
2016-09-24 18:29:53 -0400

    small update

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
 	<img id = "logo" src="images/delta.png">
 	
 	<div id = "login_form">
-<<<<<<< HEAD
+
 		<form action="login.php"  method = "GET">
 
 			<h2 align="center"><span id = "login_text">Login to My Delta</span></h2>
@@ -19,11 +19,10 @@
 
 			<span id = "custom_word">Username:</span><br>
 			<input type = "text" name = "username" placeholder="Enter SkyMiles number or username"><br>
-=======
+
 		<form action="dashboard.php"  method = "GET">
 			Username:<br>
 			<input type = "text" name = "username"><br>
->>>>>>> d5ccf49f2b013ba065d2a8ccd06def31c444812e
 			
 			<span id = "custom_word">Password:</span><br>
 			<input type = "password" name = "password" placeholder="Enter password">
