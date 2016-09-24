e0d38e933b9334f6022faa0082e7da2f1c83ea68
Forrestbob <forrestbob2000@yahoo.com>
2016-09-24 19:37:11 -0400

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="login_style.css">
+	<link rel="stylesheet" type="text/css" href="dashboard_style.css">
 	<title>Dashboard | Delta</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
 
 <body>
 	<img id = "logo" src="images/delta.png">
 	
-
+	<h1>Dashboard</h1>
 	
+	<div class = "flex-container">
+		<div class = "flex-item">1</div>
+		<div class = "flex-item">2</div>
+		<div class = "flex-item">3</div>
+		<div class = "flex-item">4</div>
+		<div class = "flex-item">5</div>
+	</div>
+
 </body>
 
 
