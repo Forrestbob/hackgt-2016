81bf4d96e86e7f11d3a3b9a088ddd6b6441b168e
Lawrence Chen <the_Crimson_chen@yahoo.com>
2016-09-25 01:45:28 -0400

    set up uber page and style page

---
+++ uber_setup.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="uber_style.css">
+	<title>Set-Up Uber Ride | Delta</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+
+<body>
+	<img id = "logo" src="images/delta.png">
+
+	<div id = "dropdown_menu">
+    
+	</div>
+	
+	<h1>Dashboard</h1>
+	
+	<div id = "large_container">
+		<div class = "flex-container">
+			<div class = "flex-item">1</div>
+			<div class = "flex-item">2</div>
+			<div class = "flex-item">3</div>
+			<div class = "flex-item">4</div>
+			<div class = "flex-item">5</div>
+		</div>
+	</div>
+
+</body>
+
+
+</html>
