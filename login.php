7e218f432da136506d34fc5bb8a11cc36680671b
Forrestbob <forrestbob2000@yahoo.com>
2016-09-25 10:02:48 -0400

    css

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	<link rel="stylesheet" type="text/css" href="login_style.css">
+	<link rel="stylesheet" type="text/css" href="static/login_style.css">
 	<title>Login | Delta</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
