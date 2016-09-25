a83c7a21341e66526487593c7096f53d24166cc4
Mitchell Lee <mitchell@Users-MacBook.local>
2016-09-25 05:10:29 -0400

    s

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect, session, flash, g, json, jsonify
 from functools import wraps
+from requests.auth import HTTPBasicAuth
 import sqlite3
+from rauth import OAuth2Service
 app = Flask(__name__)
 app.database = "DeltaDB1.db"
 app.secret_key = "oh baby"
@@ -45,7 +47,8 @@ def login():
         departure= "6000 N Terminal Pkwy, Atlanta, GA 30320",
         departure_time= "Friday August 9, 9:00pm EST",
         destination= "1 World Way, Los Angeles, CA 90045",
-        arrival_time= "Saturday August 10, 12:00am EST"
+        arrival_time= "Saturday August 10, 12:00am EST",
+        
       )
       if hasattr(user[0], 'flights'):
         user[0]['flights'].append(flight)
@@ -68,30 +71,71 @@ def estimate(index):
   params1={'sensor':'false','address': session['current_user'][0]['flights'][index]['destination']}
   results1 = requests.get(url, params=params1)
   destination = results1.json()['results'][0]['geometry']['location']
-  session['current_user'][0]['flights'][index]['dest_long_lat'] = {'long':destination['lng'], 'lat':destination['lat']}
+  session['dest_long_lat'] = {'long':destination['lng'], 'lat':destination['lat']}
+
   params2={'sensor':'false','address': session['current_user'][0]['flights'][index]['departure']}
   results2 = requests.get(url, params=params2)
   departure = results2.json()['results'][0]['geometry']['location']
-  session['current_user'][0]['flights'][index]['dept_long_lat'] = {'long':departure['lng'], 'lat':departure['lat']}
+  session['dept_long_lat'] = {'long':departure['lng'], 'lat':departure['lat']}
   return render_template('estimate.html', index=index)
 
 @app.route('/search', methods=['POST'])
 def search():
   #get long lat of text input
-  params = {'start_latitude': session['current_user'][0]['flights'][index]['dept_long_lat']['lat'], 'start-longitude':session['current_user'][0]['flights'][index]['dept_long_lat']['long'], 'end_latitude': , 'end_longitude': }
-  server_token = 'ZY-pWsbJWc_bliKFKjZO3QNcNnjmbc03DKR339CE'
-  url = 'https://api.uber.com/v1/estimates/price'
-  response = requests.get(
-    url,
-    params=params,
-    headers={
-        'Authorization': 'Bearer %s' % server_token
-    }
-  )
+  #params = {'start_latitude': session['current_user'][0]['flights'][index]['dept_long_lat']['lat'], 'start-longitude':session['current_user'][0]['flights'][index]['dept_long_lat']['long'], 'end_latitude': , 'end_longitude': }
+ 
   
   return
-#@app.route('/create_ride', methods=['POST'])
-#def create_ride():
+
+@app.route('/lyft')
+def step1():
+  lyft_api = OAuth2Service(
+     client_id='TgLMk7SiXddO',
+     client_secret='SANDBOX-7_1kYCpmzzlHpTYv_z8E_PaXFFWeupgi',
+     name='HACK GT 2016',
+     authorize_url='https://api.lyft.com/oauth/authorize',
+     access_token_url='https://api.lyft.com/oauth/token',
+     base_url='https://api.lyft.com/v1/'
+     )
+  parameters = {
+    'response_type': 'code',
+    'redirect_uri':'https://1d96fd3c.ngrok.io/step2',
+    'scope': 'rides.request profile rides.read public',
+    'client_id':'TgLMk7SiXddO',
+    'state':{}
+    }
+  login_url = lyft_api.get_authorize_url(**parameters)
+  return render_template('estimate.html', login=login_url)
+
+
+@app.route('/step2', methods=['GET'])
+def step2():
+  parameters = {
+    'redirect_uri':'https://1d96fd3c.ngrok.io/step2',
+    'code': request.args.get('code'),
+    'grant_type': 'authorization_code'
+    }
+  response=requests.post(
+    'https://api.lyft.com/oauth/token',
+    auth=('TgLMk7SiXddO','SANDBOX-7_1kYCpmzzlHpTYv_z8E_PaXFFWeupgi'),
+    data=parameters
+    )
+  print response.text
+  access_token = response.json().get('access_token')
+  session['token'] = access_token
+  return access_token
+
+@app.route('/create_ride', methods=['POST'])
+def create_ride():
+  print session['dept_long_lat']['lat']
+  print session['dept_long_lat']['long']
+  print session['dest_long_lat']['lat']
+  print session['dest_long_lat']['long']
+  price = int(request.form['price'])
+  destination = request.form['destination']
+  origin = request.form['origin']
+  params = {'origin':origin, 'destination':destination, 'ride_type':'lyft'}
+  result = requests.post('https://api.lyft.com/v1/rides', params = params)
 
   
 def connect_db():
