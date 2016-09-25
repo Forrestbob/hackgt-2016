43ba050e740bba619a3f2812e4d94ab02cbdd01b
Forrestbob <forrestbob2000@yahoo.com>
2016-09-25 07:12:56 -0400

    c

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="dashboard_style.css">
+	<link rel="stylesheet" type="text/css" href="static/dashboard_style.css">
 	<title>Dashboard | Delta</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
