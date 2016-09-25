ace853a1f245b4f28e25718f21ef6d8b847b07c2
Forrestbob <forrestbob2000@yahoo.com>
2016-09-25 04:32:33 -0400

    uber page update

--- uber_setup.php
+++ uber_setup.php
@@ -6,45 +6,37 @@
 	<link rel="stylesheet" type="text/css" href="uber_style.css">
 	<title>Set-Up Uber Ride | Delta</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<script src="jquery-1.12.4.min.js"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$("#flip").click(function(){
+				$("#panel").slideDown("medium");
+			});
+		});
+	</script>
 </head>
-
 
 <body>
 	<img id = "logo" src="images/delta.png">
-
-    <style>
-        div {
-            width: 100px;
-            height: 100px;
-            background-color: red;
-            position: relative;
-            -webkit-animation-name: example; /* Chrome, Safari, Opera */
-            -webkit-animation-duration: 4s; /* Chrome, Safari, Opera */
-            animation-name: example;
-            animation-duration: 4s;
-        }
-
-        /* Chrome, Safari, Opera */
-        @-webkit-keyframes example {
-            0%   {background-color:red; left:0px; top:0px;}
-            25%  {background-color:yellow; left:200px; top:0px;}
-            50%  {background-color:blue; left:200px; top:200px;}
-            75%  {background-color:green; left:0px; top:200px;}
-            100% {background-color:red; left:0px; top:0px;}
-        }
-
-        /* Standard syntax */
-        @keyframes example {
-            0%   {background-color:red; left:0px; top:0px;}
-            25%  {background-color:yellow; left:200px; top:0px;}
-            50%  {background-color:blue; left:200px; top:200px;}
-            75%  {background-color:green; left:0px; top:200px;}
-            100% {background-color:red; left:0px; top:0px;}
-        }
-    </style>
-
-	<div id = "dropdown_menu">
-
+	
+	<div class = "uber-form">
+	
+		<img id = "uber" src = "images/uber-full.png"><br>
+		
+		<form action = "uber_setup.php" method = "POST">
+			Enter Pickup Location:<br>
+			<input type = "text" value = "AIRPORT ADDRESS" readonly><br>
+			Enter Destination:<br>
+			<input type = "text" name = "destination"><br>
+			
+			<input type = "button" id = "flip" value = "Submit">
+			
+			<div id = "panel">
+				BLAH
+			</div>
+		
+		</form>
+		
 	</div>
 	
 
